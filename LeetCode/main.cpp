dfe29583c6ea5b4069cb0c919d303e843f511cb8
杨帅 <yancyyang@ysleep-2.local>
2017-11-11 12:23:11 +0800

    669. Trim a Binary Search Tree

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -22,33 +22,16 @@ Solution::Solution(int problemId){
 int main(int argc, const char * argv[]) {
     // insert code here...
     Solution solution(557);
-    vector<vector<int>> nums;
-    vector<int> row1;
-    row1.push_back(1);
-    row1.push_back(2);
-    row1.push_back(3);
-    row1.push_back(4);
-    row1.push_back(5);
-    row1.push_back(6);
-    vector<int> row2;
-    row2.push_back(7);
-    row2.push_back(8);
-    row2.push_back(9);
-    row2.push_back(10);
-    row2.push_back(11);
-    row2.push_back(12);
-    nums.push_back(row1);
-    nums.push_back(row2);
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(0);
+    root->right = new TreeNode(4);
     
-    vector<vector<int>> res_nums = solution.matrixReshape(nums, 3, 4);
-    for(int i=0;i<res_nums.size();i++){
-        for(int j=0;j<res_nums[i].size();j++){
-            cout<<res_nums[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    root->left->right = new TreeNode(2);
+    root->left->right->left = new TreeNode(1);
     
-
+    TreeNode* newRoot = solution.trimBST(root, 1, 2);
+    
+    int a=0;
     
     return 0;
 }
--- LeetCode/trimBST.cpp
+++ LeetCode/trimBST.cpp
@@ -6,4 +6,17 @@
 //  Copyright © 2017 杨帅. All rights reserved.
 //
 
-#include <stdio.h>
+#include "header.h"
+TreeNode* Solution:: trimBST(TreeNode* root, int L, int R){
+    if(root==NULL)
+        return NULL;
+    if(root->val<L)
+        return trimBST(root->right, L, R);
+    else if(root->val>R)
+        return trimBST(root->left, L, R);
+    else{
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root;
+    }
+}
